ee7d916bf5c66e53ebcc8aa0bbe51a7631b5ce47
Nischal Shrestha <nischalshrestha324@gmail.com>
2024-04-18 21:36:34 +0545

    Day1 of learning python

---
+++ Day1/basicArithmetic.py
@@ -0,0 +1,18 @@
+var1 = 10
+var2 = 20
+
+addition = var1 + var2
+subtraction = var2 - var1
+multiplication = var1 * var2
+division = var2 / var1
+modulus = var2 % var1
+exponentVar1 = var1 ** 2
+exponentVar2 = var2 ** 3
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division", division)
+print("Modulus:", modulus)
+print("Exponent of var1:", exponentVar1)
+print("Exponent of var2:", exponentVar2)
---
+++ Day1/variables.py
@@ -0,0 +1,5 @@
+print("To assign a variable, simple name the variable and assign a value to it")
+print("variableName = value")
+print("For example:")
+var = "Hello, World!"
+print(var)
